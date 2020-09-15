334f307fbccc86274c0f60ee7b5cddad6e8d7d9d
kyoungho.moon <moonpeter@kakao.com>
2020-09-15 19:02:24 +0900

    update 9/15

--- src/test/java/sia/tacos/TacoCloudApplicationTests.java
+++ src/test/java/sia/tacos/TacoCloudApplicationTests.java
@@ -1,8 +1,11 @@
-package sia.tacocloud;
+package sia.tacos;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class TacoCloudApplicationTests {
 
