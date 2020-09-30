cfffd7e5f8434c93eca9feb609718865e799a5a5
moonpeter <moonpeter@kakao.com>
2020-09-30 22:18:01 +0900

    update 9/30

---
+++ src/main/java/sia/tacos/web/api/TacoResource.java
@@ -0,0 +1,26 @@
+package sia.tacos.web.api;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import sia.tacos.Ingredient;
+import sia.tacos.Taco;
+
+import java.util.Date;
+import java.util.List;
+
+public class TacoResource extends ResourceSupport {
+    
+    @Getter
+    private final String name;
+
+    @Getter
+    private final Date createdAt;
+
+    @Getter
+    private final List<Ingredient> ingredients;
+
+    public TacoResource(Taco taco) {
+        this.name = taco.getName();
+        this.createdAt = taco.getCreatedAt();
+        this.ingredients = taco.getIngredients();
+    }
+}
