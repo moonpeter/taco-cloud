9784e51ea98833a8556e2f66fd3a3dc1bfda5596
moonpeter <moonpeter@kakao.com>
2020-10-05 22:49:49 +0900

    update 10/05

---
+++ src/main/java/sia/tacos/web/api/IngredientResourceAssembler.java
@@ -0,0 +1,20 @@
+package sia.tacos.web.api;
+
+import sia.tacos.Ingredient;
+
+public class IngredientResourceAssembler extends ResourceAssemblerSupport<Ingredient, Ingredientresource> {
+
+    public IngredientResourceAssembler() {
+        super(IngredientController2.class, IngredientResource.class);
+    }
+
+    @Override
+    public IngredientResource ToResource(Ingredient ingredient) {
+        return createResourceWithId(ingredient.getId(), ingredient);
+    }
+
+    @Override
+    protected IngredientResource ingredientResource(Ingredient ingredient) {
+        return new IngredientResource(ingredient);
+    }
+}
